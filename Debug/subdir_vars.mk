3535243dadca861ec84b8f3e76d9f17f084fc642
HuyDinh <dinhquochuy.eeit@gmail.com>
2016-03-10 11:35:05 +0700

    First upload

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../enet_lwip_ccs.cmd 
+
+C_SRCS += \
+../enet_lwip.c \
+../startup_ccs.c 
+
+OBJS += \
+./enet_lwip.obj \
+./startup_ccs.obj 
+
+C_DEPS += \
+./enet_lwip.pp \
+./startup_ccs.pp 
+
+C_DEPS__QUOTED += \
+"enet_lwip.pp" \
+"startup_ccs.pp" 
+
+OBJS__QUOTED += \
+"enet_lwip.obj" \
+"startup_ccs.obj" 
+
+C_SRCS__QUOTED += \
+"../enet_lwip.c" \
+"../startup_ccs.c" 
+
+
